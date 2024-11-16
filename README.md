449d1413e7519d7878530361bc356fbe8d5bac7b
应卓 <yingzhor@gmail.com>
2024-11-16 13:21:02 +0800

    2024-11-16 13:21:02

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 ## spring-boot-playground
 
+### wiki
+[启动参数](./wiki/本程序启动参数.md)
+
 ### 许可证
 
 * [Apache-2.0](.github/LICENSE.txt)
--- playground-core/src/main/docker/docker-entrypoint.sh
+++ playground-core/src/main/docker/docker-entrypoint.sh
@@ -2,6 +2,9 @@
 
 exec java \
   -XX:+PrintCommandLineFlags \
+  -XX:InitialHeapSize=4g \
+  -XX:MaxHeapSize=4g \
+  -XX:ThreadStackSize=1m \
   -Djava.security.egd=file:/dev/./urandom \
   -jar /opt/application.jar \
   "$@"
