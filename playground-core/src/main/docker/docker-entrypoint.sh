6f348c4d7439f849c10bef74703814dd8ebccb19
应卓 <yingzhor@gmail.com>
2024-11-18 14:00:23 +0800

    2024-11-18 14:00:22

--- playground-core/src/main/docker/docker-entrypoint.sh
+++ playground-core/src/main/docker/docker-entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 exec java \
+  -server \
+  -Xmixed \
   -XX:+PrintCommandLineFlags \
+  -XX:-PrintFlagsInitial \
+  -XX:+PrintFlagsFinal \
   -XX:InitialHeapSize=4g \
   -XX:MaxHeapSize=4g \
   -XX:ThreadStackSize=1m \
