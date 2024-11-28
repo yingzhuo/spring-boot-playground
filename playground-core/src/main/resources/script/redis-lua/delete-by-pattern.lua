fc77360b960abc5400e173926377f197564eec3f
应卓 <yingzhor@gmail.com>
2024-11-28 14:40:48 +0800

    2024-11-28 14:40:48

--- playground-core/src/main/resources/script/redis-lua/delete-by-pattern.lua
+++ playground-core/src/main/resources/script/redis-lua/delete-by-pattern.lua
@@ -1,6 +1,8 @@
 --[[
 被脚本用来删除符合模式的Key
 
+作者: 应卓
+参数: ARGV[1]: 要匹配的模式
 返回: 删除的Key的个数 (string)
 --]]
 
@@ -11,7 +13,7 @@ repeat
     local keys = scanResult[2];
     for i = 1, #keys do
         local key = keys[i];
-        redis.replicate_commands();
+        redis.replicate_commands(); -- 兼容Redis7.0以下版本
         redis.call("DEL", key);
         count = count + 1;
     end ;
