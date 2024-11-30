6e29e464d0a4d2c1dd918add2c8352e3bb812ce5
应卓 <yingzhor@gmail.com>
2024-11-30 15:53:30 +0800

    2024-11-30 15:53:30

--- playground-core/src/main/java/com/github/yingzhuo/playground/ApplicationBootRedis.java
+++ playground-core/src/main/java/com/github/yingzhuo/playground/ApplicationBootRedis.java
@@ -1,40 +1,41 @@
 package com.github.yingzhuo.playground;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.core.script.RedisScript;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
 
 @Configuration
 public class ApplicationBootRedis {
 
     @Bean
-    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
-        var bean = new RedisTemplate<String, String>();
-        bean.setConnectionFactory(connectionFactory);
-        bean.setDefaultSerializer(RedisSerializer.string());
-        bean.setKeySerializer(RedisSerializer.string());
-        bean.setValueSerializer(RedisSerializer.string());
-        bean.setHashKeySerializer(RedisSerializer.string());
-        bean.setHashValueSerializer(RedisSerializer.string());
-        return bean;
-    }
-
-    @Bean
     public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory connectionFactory) {
         return new StringRedisTemplate(connectionFactory);
     }
 
-    // LUA 脚本
-    // -----------------------------------------------------------------------------------------------------------------
-
     @Bean
-    public RedisScript<String> deleteByPattern() {
-        return RedisScript.of(new ClassPathResource("script/redis-lua/delete-by-pattern.lua"), String.class);
+    public RedisTemplate<String, Object> stringObjectTemplate(RedisConnectionFactory connectionFactory, ObjectMapper objectMapper) {
+
+        // @formatter:off
+        var objectMapperInUse = objectMapper.copy()
+                .configure(SerializationFeature.INDENT_OUTPUT, false);  // 不要缩进以减少Redis内存消耗
+        // @formatter:on
+
+        var serializer = new Jackson2JsonRedisSerializer<>(objectMapperInUse, Object.class);
+
+        var bean = new RedisTemplate<String, Object>();
+        bean.setConnectionFactory(connectionFactory);
+        bean.setDefaultSerializer(RedisSerializer.string());
+        bean.setKeySerializer(RedisSerializer.string());
+        bean.setValueSerializer(serializer);
+        bean.setHashKeySerializer(RedisSerializer.string());
+        bean.setHashValueSerializer(serializer);
+        return bean;
     }
 
 }
