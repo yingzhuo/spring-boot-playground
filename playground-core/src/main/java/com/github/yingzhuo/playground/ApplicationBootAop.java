cc00782e8c374582d44bfa82ecba6ae090fbc9c1
应卓 <yingzhor@gmail.com>
2024-12-07 20:15:21 +0800

    2024-12-07 20:15:21

--- playground-core/src/main/java/com/github/yingzhuo/playground/ApplicationBootAop.java
+++ playground-core/src/main/java/com/github/yingzhuo/playground/ApplicationBootAop.java
@@ -1,9 +1,19 @@
 package com.github.yingzhuo.playground;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.core.Ordered;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import spring.turbo.module.redis.aspect.AvoidRepeatedInvocationAdvice;
 
 @Configuration
 @EnableAspectJAutoProxy
 public class ApplicationBootAop {
+
+    @Bean
+    public AvoidRepeatedInvocationAdvice avoidRepeatedInvocationAdvice(StringRedisTemplate redisTemplate) {
+        return new AvoidRepeatedInvocationAdvice(redisTemplate, RuntimeException::new, Ordered.HIGHEST_PRECEDENCE);
+    }
+
 }
