70449d55833a99dac517964db00b186194f2ddcb
应卓 <yingzhor@gmail.com>
2024-12-27 19:09:43 +0800

    2024-12-27 19:09:43

--- playground-core/src/main/java/com/github/yingzhuo/playground/ApplicationBootMvc.java
+++ playground-core/src/main/java/com/github/yingzhuo/playground/ApplicationBootMvc.java
@@ -3,6 +3,7 @@ package com.github.yingzhuo.playground;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -11,6 +12,11 @@ public class ApplicationBootMvc implements WebMvcConfigurer {
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
     }
 
 }
