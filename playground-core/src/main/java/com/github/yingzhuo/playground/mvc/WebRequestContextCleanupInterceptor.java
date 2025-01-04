20d5be6a8309b51613bc40d98165ea8e8e83a917
应卓 <yingzhor@gmail.com>
2025-01-05 04:33:23 +0800

    2025-01-05 04:33:23

--- playground-core/src/main/java/com/github/yingzhuo/playground/mvc/WebRequestContextCleanupInterceptor.java
+++ playground-core/src/main/java/com/github/yingzhuo/playground/mvc/WebRequestContextCleanupInterceptor.java
@@ -3,16 +3,16 @@ package com.github.yingzhuo.playground.mvc;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.lang.Nullable;
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
 import spring.turbo.module.jdbc.ds.RoutingDataSourceLookup;
 
 @Slf4j
 public class WebRequestContextCleanupInterceptor implements HandlerInterceptor {
 
     @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) {
-        log.trace("清理Web请求上下文");
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) {
+        log.debug("清理Web请求上下文");
         RoutingDataSourceLookup.remove();
     }
 
